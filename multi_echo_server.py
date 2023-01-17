fe4b94c7cf37cbef4fbcd2a90db3816ebb391ec0
harshpp707 <73919744+harshpp707@users.noreply.github.com>
2023-01-16 20:21:59 -0700

    lab 2 done

---
+++ multi_echo_server.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+import socket
+from multiprocessing import Process
+
+# define global address and buffer size
+HOST = ""
+PORT = 8001
+BUFFER_SIZE = 1024
+
+def main():
+    
+    #create socket
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        # allow reused addresses
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        #bind socket to address
+        s.bind((HOST, PORT))
+        #set to listening mode
+        s.listen(2)
+
+        while True:
+            #connect proxy_start 
+            conn, addr = s.accept() 
+            p = Process(target=handle_AnotherProcess, args=(addr, conn))
+            p.daemon = True
+            p.start()
+            print("Started process ", p)
+
+                
+def handle_AnotherProcess(addr, conn):
+    print("Connect by", addr)
+    #recieve data, wait, then send it back
+    full_data = conn.recv(BUFFER_SIZE)
+    conn.sendall(full_data)
+    conn.shutdown(socket.SHUT_RDWR)
+    conn.close()
+
+if __name__ == "__main__":
+    main()
---
+++ multi_proxy_server.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+import socket
+import sys
+from multiprocessing import Process
+
+# define global address and buffer size
+HOST = ""
+PORT = 8001
+BUFFER_SIZE = 1024
+
+
+def get_remote_ip(host):
+    print(f'Getting IP for {host}')
+    try:
+        remote_ip = socket.gethostbyname(host)
+    except socket.gaierror:
+        print('Hostname could not be resolved. Exiting')
+        sys.exit()
+    print(f'Ip address of {host} is {remote_ip}')
+    return remote_ip
+
+def main():
+    
+    host = 'www.google.com'
+    port = 80
+
+    #create socket
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as proxy_start:
+        print("Starting proxy server")
+        # allow reused addresses, bind, and set to listening mode
+        proxy_start.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        proxy_start.bind((HOST, PORT))
+        proxy_start.listen(1)
+
+        while True:
+            #connect proxy_start 
+            conn, addr = proxy_start.accept() 
+            print("Connected by", addr) 
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as proxy_end:
+                print("Connecting to Google") 
+                remote_ip = get_remote_ip(host) 
+                #connect proxy end 
+                proxy_end.connect( (remote_ip , port) ) 
+
+                p = Process(target=send_data, args=(conn, proxy_end))
+                p.daemon = True
+                p.start()
+                print("Started process ", p)
+
+            conn.close()
+
+def send_data(conn, proxy_end):
+    #send data 
+    send_full_data = conn.recv(BUFFER_SIZE)
+    print(f"Sending recieved data {send_full_data} to google") 
+    proxy_end.sendall(send_full_data) 
+        
+    #remember to shut down!! 
+    proxy_end.shutdown(socket.SHUT_WR) 
+
+    data = proxy_end.recv(BUFFER_SIZE)
+    print(f"Sending received data {data} to client")
+    #send data back
+    conn.send(data)
+
+if __name__ == "__main__":
+    main()
